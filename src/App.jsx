222dc32214d41d3c4904cfd2a5796e3119d4dd81
aswin <aswinsrinivasan2004@gmail.com>
2023-05-14 17:51:27 +0530

    basname

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   };
   // console.log(currentUser);
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <Routes>
         <Route path="/">
           <Route
